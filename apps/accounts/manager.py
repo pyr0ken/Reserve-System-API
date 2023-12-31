8804022d316f07ed603a8815e2a0f08e8ac23808
mohammad <ramzyaghoo@gmail.com>
2023-12-31 11:23:27 +0330

    fix craetesuperuser bug

--- apps/accounts/manager.py
+++ apps/accounts/manager.py
@@ -29,7 +29,6 @@ class UserManager(BaseUserManager):
             password=password,
         )
         user.is_admin = True
-        user.is_staff = True
         user.is_superuser = True
         user.save(using=self._db)
         return user
