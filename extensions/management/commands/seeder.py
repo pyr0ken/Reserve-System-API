77c29d382506eb0fe32a584e351a9d3c22f712fd
mohammad <ramzyaghoo@gmail.com>
2024-02-24 12:35:55 +0330

    [update] seeder.py

--- extensions/management/commands/seeder.py
+++ extensions/management/commands/seeder.py
@@ -8,9 +8,19 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
 
-        SonsTimes.objects.get_or_create(time="10:00", price=100_000, holiday_price=200_000)
+        SonsTimes.objects.get_or_create(time="01:30", price=100_000, holiday_price=200_000)
+        SonsTimes.objects.get_or_create(time="07:30", price=100_000, holiday_price=200_000)
+        SonsTimes.objects.get_or_create(time="09:00", price=100_000, holiday_price=200_000)
+        SonsTimes.objects.get_or_create(time="10:30", price=100_000, holiday_price=200_000)
         SonsTimes.objects.get_or_create(time="12:00", price=100_000, holiday_price=200_000)
+        SonsTimes.objects.get_or_create(time="13:30", price=100_000, holiday_price=200_000)
         SonsTimes.objects.get_or_create(time="15:00", price=100_000, holiday_price=200_000)
+        SonsTimes.objects.get_or_create(time="16:30", price=100_000, holiday_price=200_000)
+        SonsTimes.objects.get_or_create(time="18:00", price=100_000, holiday_price=200_000)
+        SonsTimes.objects.get_or_create(time="19:30", price=100_000, holiday_price=200_000)
+        SonsTimes.objects.get_or_create(time="21:00", price=100_000, holiday_price=200_000)
+        SonsTimes.objects.get_or_create(time="22:30", price=100_000, holiday_price=200_000)
+        SonsTimes.objects.get_or_create(time="23:59", price=100_000, holiday_price=200_000)
 
         self.stdout.write(
             self.style.SUCCESS('SonsTimes added successfully')
